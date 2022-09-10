cb1200732c8c902f98917bfc9b00197577c8fa16
FranzCortez <franzco2000@gmail.com>
2022-09-10 17:14:20 -0300

    chore(index): cors y conexion

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 import publicRoutes from "./routes/publicRoutes.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
@@ -7,11 +9,32 @@ import db from "./config/db.js";
 //App
 const app = express();
 
-// Habilitar lectura de datos
-app.use( express.urlencoded({ extended: true }));
-
 // Carpeta publica
 app.use(express.static('public'));
+
+// Habilitar lectura de datos
+app.use( bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.json());
+
+// Habilitar cors
+const whitelist = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: (origin, cb) => {
+        console.log(origin);
+        
+        const existe = whitelist.some( dominio => {
+            console.log(dominio)
+            return dominio === origin
+        });
+        
+        if( existe) {
+            cb(null, true);
+        } else {
+            cb(new Error('No permitido por CORS'));
+        }
+    }
+}
+app.use(cors(corsOptions));
 
 // Conexion DB
 try {
@@ -28,7 +51,8 @@ app.use('/api/cuentas', usuarioRoutes);
 
 // Puerto
 const port = process.env.PORT || 5000;
+const host = process.env.HOST || '0.0.0.0';
 
-app.listen( port, () => {
+app.listen( port, host, () => {
     console.log(`Server corriendo en el puerto ${port}`);
 });
