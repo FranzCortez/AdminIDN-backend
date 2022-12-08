7d7d74f245c903b35b16cca27a24399f6c6e263b
FranzCortez <franzco2000@gmail.com>
2022-12-08 11:47:32 -0300

    feat(factura): campos con null

--- models/Factura.js
+++ models/Factura.js
@@ -4,42 +4,43 @@ import db from "../config/db.js";
 const Factura = db.define('factura', {
     numeroFactura: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: true
     },
     fechaFactura: {
         type: DataTypes.DATEONLY,
-        allowNull: false
+        allowNull: true
     },
     numeroCompra: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: true
     },
     fechaCompra: {
         type: DataTypes.DATEONLY,
-        allowNull: false
+        allowNull: true
     },
     formaPago: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: true
     },
     monto: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: true
     },
     fechaPago: {
         type: DataTypes.DATEONLY,
+        allowNull: true
     },
     observaciones: {
         type: DataTypes.TEXT
     },
     estado: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         defaultValue: 0
     },
     guiaDespacho: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: true
     },
     fechaGuiaDespacho: {
         type: DataTypes.DATEONLY,
